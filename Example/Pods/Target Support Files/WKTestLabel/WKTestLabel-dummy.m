66fa0f34abddaebb52af24ce94e63b8f41ba1a57
WuKongCoo1 <wukong@wukedeMac-mini.local>
2016-10-20 09:21:38 +0800

    0.1.0
    0.1.0

---
+++ Example/Pods/Target Support Files/WKTestLabel/WKTestLabel-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WKTestLabel : NSObject
+@end
+@implementation PodsDummy_WKTestLabel
+@end
---
+++ Example/Pods/Target Support Files/WKTestLabel/WKTestLabel-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double WKTestLabelVersionNumber;
+FOUNDATION_EXPORT const unsigned char WKTestLabelVersionString[];
+

597c8901bb3efc5285e33567e3831c13b2c78728
WuKongCoo1 <wukong@wukedeMac-mini.local>
2016-10-20 09:26:42 +0800

    add test label

---
+++ WKTestLabel/Classes/WKTestLabel.h
@@ -0,0 +1,13 @@
+//
+//  WKTestLabel.h
+//  Pods
+//
+//  Created by 吴珂 on 2016/10/20.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WKTestLabel : UILabel
+
+@end
---
+++ WKTestLabel/Classes/WKTestLabel.m
@@ -0,0 +1,21 @@
+//
+//  WKTestLabel.m
+//  Pods
+//
+//  Created by 吴珂 on 2016/10/20.
+//
+//
+
+#import "WKTestLabel.h"
+
+@implementation WKTestLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
